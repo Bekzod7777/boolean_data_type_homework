976358b1d2268514afc327c0e70168640646f4a8
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-18 17:05:26 +0500

    8m

--- bool08.py
+++ bool08.py
@@ -1,4 +1,5 @@
 def main(a):
+    a=int(a)
     """
     check the whole number. Integers are 0 and a positive number.
     Args:
@@ -7,4 +8,4 @@ def main(a):
         bool
     """
     # Write your code here
-    return
+    return a>=0
