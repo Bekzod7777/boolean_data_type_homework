ada3ac825070f87c328c51a3548faa27f45399bd
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-18 16:10:26 +0500

    4m

--- bool04.py
+++ bool04.py
@@ -1,4 +1,5 @@
 def main(a):
+    a=int(a)
     """
     Check the logic "The variable "a" is negative"
     Args:
@@ -7,4 +8,4 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
+    return a<0
